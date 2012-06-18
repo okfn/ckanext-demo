731c6003ab4b61cd26a971c7e1b12dd56368f7c7
Toby <toby.junk@gmail.com>
2012-06-18 17:33:30 +0100

    initial commit

---
+++ ckanext/demo/plugins.py
@@ -0,0 +1,10 @@
+import ckan.plugins as p
+
+
+class Demo(p.SingletonPlugin):
+
+    p.implements(p.IConfigurer)
+
+    def update_config(self, config):
+        # add template directory
+        p.toolkit.add_template_directory(config, 'templates')
