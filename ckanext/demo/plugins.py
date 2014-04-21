71b373200c59702a9c30bebca2c4574b6579aed9
nigelb <nigelbabu@gmail.com>
2014-04-21 14:04:08 +0530

    Update to the version in development

--- ckanext/demo/plugins.py
+++ ckanext/demo/plugins.py
@@ -6,7 +6,7 @@ class Demo(p.SingletonPlugin):
     p.implements(p.IConfigurer)
 
     def update_config(self, config):
-        if p.toolkit.check_ckan_version(min_version='2.0'):
-            # add template directory
-            p.toolkit.add_template_directory(config, 'templates')
-            p.toolkit.add_public_directory(config, 'public')
+        # add template directory
+        p.toolkit.add_template_directory(config, 'templates')
+        p.toolkit.add_public_directory(config, 'public')
+        p.toolkit.add_resource('fanstatic_library', 'ckanext-demo')
