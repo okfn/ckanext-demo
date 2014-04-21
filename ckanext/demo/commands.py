e7feabe1715681a6376a2d71f0f4a4a2f8fb690c
nigelb <nigelbabu@gmail.com>
2014-04-21 11:55:49 +0530

    Also delete package_relationship

--- ckanext/demo/commands.py
+++ ckanext/demo/commands.py
@@ -124,7 +124,12 @@ class Reset(CkanCommand):
         delete from package_extra where package_id in (select id from package where state = 'to_delete');
         delete from member where table_id in (select id from package where state = 'to_delete');
         delete from resource_group where package_id in (select id from package where state = 'to_delete');
+
         delete from related_dataset where dataset_id in (select id from package where state = 'to_delete');
+        delete from package_relationship_revision where subject_package_id in (select id from package where state = 'to_delete');
+        delete from package_relationship_revision where object_package_id in (select id from package where state = 'to_delete');
+        delete from package_relationship where subject_package_id in (select id from package where state = 'to_delete');
+        delete from package_relationship where object_package_id in (select id from package where state = 'to_delete');
 
         delete from package where id in (select id from package where state = 'to_delete'); commit;'''
         model.Session.execute(sql)
